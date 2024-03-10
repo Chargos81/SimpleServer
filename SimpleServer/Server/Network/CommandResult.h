925d0f4a4186cafb87e459a9a850ebc25d49895c
HOME-PC\Nevermore <chargos81@gmail.com>
2024-03-10 09:23:44 +0300

    Base network types

---
+++ SimpleServer/Server/Network/CommandResult.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <string>
+
+namespace server::network
+{
+	class CommandResult
+	{
+	public:
+
+		explicit CommandResult(std::string key, std::string value) noexcept
+			: Key(std::move(key)),
+			  Value(std::move(value))
+		{}
+
+		std::string Key;
+		std::string Value;
+	};
+}
---
+++ SimpleServer/Server/Network/INetworkManager.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+namespace server
+{
+	class ServerApplication;
+}
+
+namespace server::network
+{
+	class CommandResult;
+
+	class INetworkManager
+	{
+	public:
+
+		virtual void Initialize(ServerApplication* application) = 0;
+		virtual void Send(const CommandResult& commandResult) = 0;
+
+		virtual ~INetworkManager() = default;
+	};
+}
